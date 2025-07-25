aa2abfa8f0238848f75d87d628340b2262ebc9d9
swapnilverma25 <swapnilverma25@gmail.com>
2025-07-25 21:06:38 +0530

    add gemini code

--- gemini-hr-assistant/app.py
+++ gemini-hr-assistant/app.py
@@ -12,17 +12,29 @@ for file in os.listdir("data"):
     if file.endswith(".txt"):
         loader = TextLoader(os.path.join("data", file))
         docs.extend(loader.load())
-
+print("A")
+print(docs)
+print("B")
 # Step 2: Split into chunks
 splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 chunks = splitter.split_documents(docs)
-
+print("C")
+print(chunks)
+print("D")
+print(len(chunks))
+print("E")
 # Step 3: Create embeddings using Gemini/VertexAI
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:\Users\swapn\PycharmProjects\AIProject\gemini-hr-assistant\genai-hr-bot-dd17eebdd3fc.json"
+print("F")
+from vertexai import init
+print("G")
+init(project="genai-hr-bot", location="us-central1")
+print("H")
 embeddings = VertexAIEmbeddings(model_name="textembedding-gecko@001")
-
+print("I")
 # Step 4: Save embeddings to Chroma
 vectorstore = Chroma.from_documents(chunks, embeddings, persist_directory="chroma_db")
-
+print("D")
 # Step 5: Set up Q&A chain using Gemini Chat Model
 retriever = vectorstore.as_retriever()
 llm = ChatVertexAI(model_name="gemini-pro", temperature=0)
